ad6de5f4c216fdda04a41c468b3537900459afab
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-02-12 23:39:17 +0600

    added basic project and structured the code

--- src/body/Body.jsx
+++ src/body/Body.jsx
@@ -1,19 +1,17 @@
-import nayan from "../images/nayan.jpeg"
-import './Body.css'
+import nayan from "../images/nayan.webp";
+import "./Body.css";
 
-function Body(){
-    return (
-        <div className='body'>
-            <div className='details'>
-                <h3>
-                    Hello, This is nayan.
-                </h3>
-            </div>
-            <div className='image'>
-                <img src={nayan} alt="nayan" />
-            </div>
-        </div>
-    );
+function Body() {
+  return (
+    <div className="body">
+      <div className="details">
+        <h3>Hello, This is Nayan.</h3>
+      </div>
+      <div className="image">
+        <img src={nayan} alt="nayan" />
+      </div>
+    </div>
+  );
 }
 
 export default Body;
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,13 +1,13 @@
-import './Footer.css'
+import "./Footer.css";
 
-function Footer(){
-    return (
-        <div className='footer'>
-            <h1>Get in touch</h1>
-            <p>Mobile: 01930264879</p>
-            <p>Email: mehadihasannayan1998@gmail.com</p>
-        </div>
-    );
+function Footer() {
+  return (
+    <div className="footer">
+      <h1>Get in touch</h1>
+      <p>Mobile: 01930264879</p>
+      <p>Email: mehadihasannayan1998@gmail.com</p>
+    </div>
+  );
 }
 
 export default Footer;
